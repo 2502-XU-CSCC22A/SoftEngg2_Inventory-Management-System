c6ccf7672ba46225db084ab5673e99851a162f4f
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-05-01 16:39:51 +0800

    Add hooks for fetching all transactions, including voided

--- frontend/src/hooks/useTransactions.js
+++ frontend/src/hooks/useTransactions.js
@@ -2,11 +2,26 @@ import { useQuery } from '@tanstack/react-query';
 import api from '../api/api.js';
 
 export const useTransactions = () => {
-    return useQuery({
+    const query = useQuery({
         queryKey: ['transactions'],
-        queryFn: async () => {
-            const { data } = await api.get('/transactions');
+        queryFn: async () => { 
+            const { data } = await api.get('/transactions'); 
             return data;
         },
+        onError: (error) => {
+            console.error("Error fetching transactions:", error);
+        },
     });
+
+    const queryAll = useQuery({
+        queryKey: ['transactions', 'all'],
+        queryFn: async () => {
+            const { data } = await api.get('/transactions/show-all');
+            return data;
+        },
+        onError: (error) => {
+            console.error("Error fetching all transactions:", error);   
+        },
+    });
+    return { query, queryAll };
 };
