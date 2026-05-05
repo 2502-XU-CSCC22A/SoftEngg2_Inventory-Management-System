832cfff2754741a4c9617e45c24938bdd7cccf87
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-05-06 00:45:18 +0800

    Add another route for fetching all records by month and year

--- backend/src/routes/transactions.js
+++ backend/src/routes/transactions.js
@@ -4,7 +4,7 @@ export const transactionsRouter = express.Router();
 
 import models, { sequelize } from "../config/db.js";
 import { carvePayload, validate, validateMonthAndYear } from "../middleware/transactionsMiddleware.js";
-import { getTransactionByMonthAndYear, insertTransaction, updateTransaction } from "../controllers/transactionsController.js";
+import { getTransactionByMonthAndYear, insertTransaction, updateTransaction, getAllTransactionsByMonthAndYear } from "../controllers/transactionsController.js";
 import { insertTransactionSchema, updateTransactionSchema } from "../schemas/schemas.js";
 import { fetchUpdateableTransaction } from "../middleware/transactionsMiddleware.js";
 
@@ -63,6 +63,21 @@ transactionsRouter.post('/', validate(insertTransactionSchema), async (req, res,
   }
   catch (error) {
     console.error("Error in insert: ", error);
+    return res.status(error.status || 500).json({ message: error.message });
+  }
+})
+
+// get ALL transactions by month and year (for logs)
+transactionsRouter.get('/show-all/filter', validateMonthAndYear, async (req, res) => {
+  const { month, year } = req.query;
+  try {
+    const transactions = await getAllTransactionsByMonthAndYear(month, year);
+    if (transactions) {
+      return res.status(200).json({ message: "Data fetched successfully.", data: transactions })
+    }
+  }
+  catch (error) {
+    console.error(`Error in fetch txn by date: ${error}`);
     return res.status(error.status || 500).json({ message: error.message });
   }
 })
